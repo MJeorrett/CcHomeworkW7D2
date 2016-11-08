2e8ced730f33c88f498366ef2dc56f1c06569418
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-08 17:52:49 +0000

    adding advancedWordCount

--- app/src/main/java/com/example/user/cchomeworkw7d2/WordCounter.java
+++ app/src/main/java/com/example/user/cchomeworkw7d2/WordCounter.java
@@ -1,17 +1,51 @@
 package com.example.user.cchomeworkw7d2;
 
+import java.util.HashMap;
+
 /**
  * Created by user on 08/11/2016.
  */
 
 public class WordCounter {
 
-    public static int simpleCountWords(String text) {
+    public static int simpleCountWords( String text ) {
 
         if ( text.isEmpty() ) {
             return 0;
         }
 
-        return text.split("\\s+").length;
+        String[] words = WordCounter.getWordsFromString( text );
+        return words.length;
+    }
+
+    public static HashMap<String, Integer> advancedCountWords( String text ) {
+
+        if ( text.isEmpty() ) {
+            return null;
+        }
+
+        String[] words = WordCounter.getWordsFromString( text );
+        HashMap<String, Integer> wordsCount = new HashMap<String, Integer>();
+
+        for ( String word : words ) {
+
+            Integer newCount = 1;
+
+            if ( wordsCount.containsKey( word ) ) {
+                Integer existingCount = wordsCount.get( word );
+                newCount = existingCount + 1;
+            }
+
+            wordsCount.put( word, newCount );
+        }
+
+        return wordsCount;
+    }
+
+    private static String[] getWordsFromString( String string ) {
+
+        String cleanString = string.replaceAll( "[,.]", "" );
+        String[] words = cleanString.split( "\\s+");
+        return words;
     }
 }
--- app/src/test/java/com/example/user/cchomeworkw7d2/WordCounterTest.java
+++ app/src/test/java/com/example/user/cchomeworkw7d2/WordCounterTest.java
@@ -1,6 +1,9 @@
 package com.example.user.cchomeworkw7d2;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,18 +13,55 @@ import static org.junit.Assert.assertEquals;
 
 public class WordCounterTest {
 
+    HashMap<String, Integer> expectedHashMap;
+
+    @Before
+    public void before() {
+
+        this.expectedHashMap = new HashMap<String, Integer>();
+    }
+
     @Test
-    public void emptyString() {
+    public void simpleCountEmptyString() {
         assertEquals( 0, WordCounter.simpleCountWords( "" ) );
     }
 
     @Test
-    public void oneWord() {
+    public void simpleCountOneWord() {
         assertEquals( 1, WordCounter.simpleCountWords( "one" ) );
     }
 
     @Test
-    public void multipleWords() {
+    public void simpleCountMultipleWords() {
         assertEquals( 4, WordCounter.simpleCountWords( "There are multiple words" ) );
+    }
+
+    @Test
+    public void advancedCountEmptyString() {
+        assertEquals( null, WordCounter.advancedCountWords( "" ) );
+    }
+
+    @Test
+    public void advancedCountOneWord() {
+
+        expectedHashMap.put( "test", 1 );
+
+        assertEquals( expectedHashMap, WordCounter.advancedCountWords( "test" ) );
+    }
+
+    @Test
+    public void advancedCountMultipleWords() {
+        String testString = "three times three is three, no only kidding it is nine ";
+
+        expectedHashMap.put( "three", 3 );
+        expectedHashMap.put( "times", 1 );
+        expectedHashMap.put( "is", 2 );
+        expectedHashMap.put( "no", 1 );
+        expectedHashMap.put( "only", 1 );
+        expectedHashMap.put( "kidding", 1 );
+        expectedHashMap.put( "it", 1 );
+        expectedHashMap.put( "nine", 1 );
+
+        assertEquals( expectedHashMap, WordCounter.advancedCountWords( testString ) );
     }
 }
